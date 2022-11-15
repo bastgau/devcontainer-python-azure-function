48fa4a7a3b8820fa3c2cfc168417aad8b22e599e
Bastien Gautier <bastien.gautier@dcube.fr>
2022-11-15 17:34:11 +0100

    :bug: Change directory name

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -3,7 +3,7 @@
 sudo chgrp vscode /workspaces/app/.venv
 sudo chown vscode /workspaces/app/.venv
 
-git config --global --add safe.directory ${containerWorkspaceFolder}
+git config --global --add safe.directory /workspaces/app/
 git config --global core.autocrlf true
 
 sudo apt-get update
