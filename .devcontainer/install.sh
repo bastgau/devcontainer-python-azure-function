9e5df8d09b74ed8bc176cb46be46b60ef710faa8
Bastien Gautier <bastien@mixr.net>
2022-12-03 03:32:48 +0100

    :recycle: Move dependency installation

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -6,9 +6,6 @@ sudo chown vscode /workspaces/app/.venv
 git config --global --add safe.directory /workspaces/app/
 git config --global core.autocrlf true
 
-sudo apt-get update
-sudo apt-get install vim -y
-
 python3 -m venv /workspaces/app/.venv
 PATH="/workspaces/app/.venv/bin:$PATH"
 
